f5ebc538e49e79d746a523bc4be859959684ceb5
RICARDO PIETRO BERKEMBROCK MANDARINO <ricardo.mandarino@alunos.sc.senac.br>
2023-12-11 21:40:42 -0300

    Começando API

---
+++ MinhaApi/Contracts/Repository/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using MinhaApi.Entity;
+
+namespace MinhaApi.Contracts.Repository
+{
+    public interface IUserRepository
+    {
+        Task Add(UserEntity user);
+        Task Update(UserEntity user);
+        Task Delete(UserEntity user);
+        Task<UserEntity> GetByID(int id);
+        Task<IEnumerable<UserEntity>> Get();
+    }
+}
---
+++ MinhaApi/Repository/UserRepository.cs
@@ -0,0 +1,36 @@
+﻿using Dapper;
+using MinhaApi.Contracts.Repository;
+using MinhaApi.Entity;
+using MinhaApi.Infrastructure;
+
+namespace MinhaApi.Repository
+{
+    public class UserRepository : Connection, IUserRepository
+    {
+        public Task Add(UserEntity user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(UserEntity user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<UserEntity>> Get()
+        {
+                string sql = "SELECT * FROM USER";
+                return await GetConnection().QueryAsync<UserEntity>(sql);
+        }
+
+        public Task<UserEntity> GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(UserEntity user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
